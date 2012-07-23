701346f452213e96209e036189f7ac9585df56ba
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-07-23 11:26:33 -0300

    Adicionado função para restartar o passenger no deploy. Teste alterando mensagem da view de index.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,3 +17,12 @@ after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
