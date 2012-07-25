c1e358410c9dd63226c88e70391fa5c71b0a1c92
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-07-25 11:03:15 -0300

    Modificado valida��es no model do raw_file e cabe�alho de valida��o do form de upload. #8

--- app/models/raw_file.rb
+++ app/models/raw_file.rb
@@ -1,2 +1,8 @@
 class RawFile < ActiveRecord::Base
+  attr_accessible :path, :tags
+
+  validates :path,  :presence => true
+  validates :tags, :presence => true,
+                    :length => { :minimum => 3 }
+
 end
