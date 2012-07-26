e78f954aaf9d8ba01ca20c7098a65047208a1373
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-07-26 20:47:31 -0300

    Alterado arquivos de ambiente de dev e prod para enviar e-mails corretamente. #6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,13 @@ TC::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'localhost:3000',
+      :user_name            => 'tcalrunip2012',
+      :password             => 'unip2012',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true
+  }
 end
