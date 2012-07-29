a27b90625784ff716390d08f658675a0ab1d760e
Adilson Roberto Leonello <aleonello@gmail.com>
2012-07-29 15:44:35 -0300

    -Criado validador de Tags. #15

--- app/models/raw_file.rb
+++ app/models/raw_file.rb
@@ -1,9 +1,10 @@
+# encoding: utf-8
 class RawFile < ActiveRecord::Base
   attr_accessible :path, :tags
 
   validates :path,  :presence => true
   validates :tags, :presence => true,
                     :length => { :minimum => 3 },
-                    :format => { :with => /(([0-9a-zA-Z]+)([,][0-9a-zA-Z]*)*)/ }
+                    :format => { :with => /^[A-z0-9_\-ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖòóôõöÈÉÊËèéêëðÇçÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž]+([,][A-z0-9_\-ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖòóôõöÈÉÊËèéêëðÇçÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž]*)*$/ }
 
 end
