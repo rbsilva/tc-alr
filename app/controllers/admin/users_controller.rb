2746e43e261aa17b1bda91685b9fb9d7396cd5b4
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-08-02 08:57:55 -0300

    Corrigido problema do admin de usuários #11

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -87,7 +87,7 @@ class Admin::UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url }
+      format.html { redirect_to admin_users_url }
       format.json { head :no_content }
     end
   end
