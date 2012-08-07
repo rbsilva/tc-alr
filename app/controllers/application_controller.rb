0033b50caa51d3e7e509f476a97bce24f5b6fb04
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-08-07 09:02:53 -0300

    Some fixes. #11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  layout "login"
-
   # Code used by CanCan to manage exception
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
 
   before_filter :authenticate_user! #, :except => [:some_action_without_auth]
 
-  layout "home"
-
   def index
   end
 end
