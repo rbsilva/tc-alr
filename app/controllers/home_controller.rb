4190192caa6b421ed7f81b405cf13f9ea8ccb890
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-08-19 15:52:33 -0300

    Identação

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-
   before_filter :authenticate_user! #, :except => [:some_action_without_auth]
+
+  caches_page :index
 
   def index
   end
 end
-
--- app/controllers/raw_files_controller.rb
+++ app/controllers/raw_files_controller.rb
@@ -2,6 +2,8 @@ class RawFilesController < ApplicationController
   before_filter :get_user
   before_filter :authenticate_user! #, :except => [:some_action_without_auth]
 
+  caches_action :index
+
   # GET /raw_files
   # GET /raw_files.json
   def index
@@ -56,110 +58,111 @@ class RawFilesController < ApplicationController
   # POST /raw_files
   # POST /raw_files.json
   def create
-	RawFile.transaction do  
-		@raw_file = RawFile.new(params[:raw_file])
-		begin
-		  require 'fileutils'
-		  filename = @raw_file.path
-		  if params[:raw_file][:attach_a_file].nil? then
-			@raw_file.status = 'SENT'
-			if @raw_file.save then
-			  save_file filename
-			  continue = 'created'
-			else
-			  continue = false
-			end
-		  else
-			tempfile = params[:raw_file][:attach_a_file].tempfile
-			original_filename = params[:raw_file][:attach_a_file].original_filename
-			@raw_file.path = attach_file filename, original_filename, tempfile
-			continue = 'attached'
-		  end
-		rescue Exception => e
-		  logger.info e
-		  @raw_file.errors.set :path, e.message
-		  continue = false
-		end
+    RawFile.transaction do
+      expire_action :action => :index
+      @raw_file = RawFile.new(params[:raw_file])
+      begin
+        require 'fileutils'
+        filename = @raw_file.path
+        if params[:raw_file][:attach_a_file].nil? then
+          @raw_file.status = 'SENT'
+          if @raw_file.save then
+            save_file filename
+            continue = 'created'
+          else
+            continue = false
+          end
+        else
+          tempfile = params[:raw_file][:attach_a_file].tempfile
+          original_filename = params[:raw_file][:attach_a_file].original_filename
+          @raw_file.path = attach_file filename, original_filename, tempfile
+          continue = 'attached'
+        end
+      rescue Exception => e
+        logger.info e
+        @raw_file.errors.set :path, e.message
+        continue = false
+      end
 
-		respond_to do |format|
-		  if continue == 'attached'
-			format.html { render action: "new" }
-			#format.json { render json: @raw_file, status: :created, location: @raw_file }
-		  elsif continue == 'created'
-			format.html { redirect_to @raw_file, notice: 'File was successfully created.' }
-			format.json { render json: @raw_file, status: :created, location: @raw_file }
-		  else
-			format.html { render action: "new" }
-			format.json { render json: @raw_file.errors, status: :unprocessable_entity }
-		  end
-		end
-	end
+      respond_to do |format|
+        if continue == 'attached'
+          format.html { render action: "new" }
+          #format.json { render json: @raw_file, status: :created, location: @raw_file }
+        elsif continue == 'created'
+          format.html { redirect_to @raw_file, notice: 'File was successfully created.' }
+          format.json { render json: @raw_file, status: :created, location: @raw_file }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @raw_file.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
 
   # PUT /raw_files/1
   # PUT /raw_files/1.json
   def update
-	RawFile.transaction do   
-		@raw_file = RawFile.find(params[:id],:conditions => ['user_id = ?',current_user.id])
-		begin
-		  require 'fileutils'
-		  filename = @raw_file.path
-		  if params[:raw_file][:attach_a_file].nil? then
-			old_filename = @raw_file.path
-			if @raw_file.update_attributes(params[:raw_file]) then
-			  save_file @raw_file.path
-			  if old_filename != @raw_file.path then
-				upload_dir = APP_CONFIG['upload_dir']
-				file = File.join(upload_dir, old_filename)
-				rm_file file
-			  end
-			  continue = 'updated'
-			else
-			  continue = false
-			end
-		  else
-			tempfile = params[:raw_file][:attach_a_file].tempfile
-			original_filename = params[:raw_file][:attach_a_file].original_filename
-			@raw_file.path = attach_file filename, original_filename, tempfile
-			continue = 'attached'
-		  end
-		rescue Exception => e
-		  logger.info e
-		  @raw_file.errors.set :path, e.message
-		  continue = false
-		end
+    RawFile.transaction do
+      @raw_file = RawFile.find(params[:id],:conditions => ['user_id = ?',current_user.id])
+      begin
+        require 'fileutils'
+        filename = @raw_file.path
+        if params[:raw_file][:attach_a_file].nil? then
+          old_filename = @raw_file.path
+          if @raw_file.update_attributes(params[:raw_file]) then
+            save_file @raw_file.path
+            if old_filename != @raw_file.path then
+              upload_dir = APP_CONFIG['upload_dir']
+              file = File.join(upload_dir, old_filename)
+              rm_file file
+            end
+            continue = 'updated'
+          else
+            continue = false
+          end
+        else
+          tempfile = params[:raw_file][:attach_a_file].tempfile
+          original_filename = params[:raw_file][:attach_a_file].original_filename
+          @raw_file.path = attach_file filename, original_filename, tempfile
+          continue = 'attached'
+        end
+      rescue Exception => e
+        logger.info e
+        @raw_file.errors.set :path, e.message
+        continue = false
+      end
 
-		respond_to do |format|
-		  if continue == 'attached'
-			format.html { render action: "edit" }
-			#format.json { render json: @raw_file, status: :created, location: @raw_file }
-		  elsif continue == 'updated'
-			format.html { redirect_to @raw_file, notice: 'Raw file was successfully updated.' }
-			format.json { head :no_content }
-		  else
-			format.html { render action: "edit" }
-			format.json { render json: @raw_file.errors, status: :unprocessable_entity }
-		  end
-		end
-	end
+      respond_to do |format|
+        if continue == 'attached'
+          format.html { render action: "edit" }
+          #format.json { render json: @raw_file, status: :created, location: @raw_file }
+        elsif continue == 'updated'
+          format.html { redirect_to @raw_file, notice: 'Raw file was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @raw_file.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
 
   # DELETE /raw_files/1
   # DELETE /raw_files/1.json
   def destroy
-	RawFile.transaction do 
-		@raw_file = RawFile.find(params[:id],:conditions => ['user_id = ?',current_user.id])
-		filename = @raw_file.path
-		if @raw_file.destroy then
-		  upload_dir = APP_CONFIG['upload_dir']
-		  file = File.join(upload_dir, filename)
-		  rm_file file
-		end
-		respond_to do |format|
-		  format.html { redirect_to raw_files_url }
-		  format.json { head :no_content }
-		end
-	end
+    RawFile.transaction do
+      @raw_file = RawFile.find(params[:id],:conditions => ['user_id = ?',current_user.id])
+      filename = @raw_file.path
+      if @raw_file.destroy then
+        upload_dir = APP_CONFIG['upload_dir']
+        file = File.join(upload_dir, filename)
+        rm_file file
+      end
+      respond_to do |format|
+        format.html { redirect_to raw_files_url }
+        format.json { head :no_content }
+      end
+    end
   end
 
   private
