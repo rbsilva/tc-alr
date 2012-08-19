c6a1be8f3ffe1d6be5e153320dce0ba800cbdedf
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-08-18 22:16:59 -0300

    Fix #11 Não pode mais se cadastrar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name, :role_ids, :user, :delete_flag, :active_flag
