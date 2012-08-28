10f8bc7fc973c9d8325bb4ea91eba42c6eb09928
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-08-28 20:03:49 -0300

    Modificação para CSV #18

--- app/models/raw_file_transformer.rb
+++ app/models/raw_file_transformer.rb
@@ -12,76 +12,56 @@ end
 
 class RawFileTransformer < ActiveRecord::Base
   def self.transform
+    require 'csv'
+    require 'yaml'
+
     begin
       upload_dir = APP_CONFIG['upload_dir']
-	  processed_dir = APP_CONFIG['processed_dir']
-	  inbound_dir = APP_CONFIG['inbound_dir']
+    processed_dir = APP_CONFIG['processed_dir']
+    inbound_dir = APP_CONFIG['inbound_dir']
       if !Dir.exists? processed_dir then
         FileUtils.makedirs processed_dir
       end
-	  if !Dir.exists? inbound_dir then
+    if !Dir.exists? inbound_dir then
         FileUtils.makedirs inbound_dir
       end
-	  
-	  @raw_files = RawFile.where("status = 'SENT'")
-	  @raw_files.each do |raw_file| 
-		begin
-			filename = raw_file.path
-			file = File.join(upload_dir, filename)
-			processed_file = File.join(processed_dir, filename)
-			filename_yml = filename + '.yml'
-			inbound_file = File.join(inbound_dir, filename_yml)
-		  
-			case File.extname(file)
-			  when '.xls'
-				inbound = Excel.new(file)
-			  when '.csv'
-				inbound = CSV.new(file)
-			  else
-			end
-		  
-		   
-		  content = inbound.to_yaml
 
-		  if inbound.is_a? Excel then
-			inbound.get_workbook.get_io.close
-		  end
-		  
-		  File.open(inbound_file, 'w') {|f| f.write(content) }
-		  raw_file.status = 'PROCESSED'
-		  raw_file.save
-		  FileUtils.mv file, processed_file
-		  ActiveRecord::Base.connection.close
-		  true
-		rescue Exception => e
-		  logger.fatal e
-		end
-	  end
+    @raw_files = RawFile.where("status = 'SENT'")
+    @raw_files.each do |raw_file|
+    begin
+      filename = raw_file.path
+      file = File.join(upload_dir, filename)
+      processed_file = File.join(processed_dir, filename)
+      filename_yml = filename + '.yml'
+      inbound_file = File.join(inbound_dir, filename_yml)
+
+      case File.extname(file)
+        when '.xls'
+          inbound = Excel.new(file)
+        when '.csv'
+          inbound = CSV.read(file).to_yaml
+        else
+      end
+
+
+      content = inbound.to_yaml
+
+      if inbound.is_a? Excel then
+      inbound.get_workbook.get_io.close
+      end
+
+      File.open(inbound_file, 'w') {|f| f.write(content) }
+      raw_file.status = 'PROCESSED'
+      raw_file.save
+      FileUtils.mv file, processed_file
+      ActiveRecord::Base.connection.close
+      true
     rescue Exception => e
-	  logger.fatal e
+      logger.fatal e
+    end
+    end
+    rescue Exception => e
+    logger.fatal e
     end
   end
-end
-
-class CSV
-	def initialize(input) 
-		require 'csv'
-		require 'yaml'
-
-		doc = CSV.read(input)
-		fields = doc.shift
-		records = Hash.new
-		doc.each_with_index do |row, i|
-		  record = Hash.new
-		  fields.each_with_index do |field, j|
-			record[field] = row[j]
-		  end
-		  records["record_#{i}"] = record
-		end
-		@records = records
-	end
-	
-	def to_yaml
-		@records.to_yaml
-	end
 end
