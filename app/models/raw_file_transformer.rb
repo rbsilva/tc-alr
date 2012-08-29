19d3542470a9aa65582a95ffdecb711383acc7bb
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-08-29 15:22:42 -0300

    testes #42

--- app/models/raw_file_transformer.rb
+++ app/models/raw_file_transformer.rb
@@ -15,39 +15,44 @@ class RawFileTransformer < ActiveRecord::Base
     require 'csv'
     require 'yaml'
 
-    begin
-      upload_dir = APP_CONFIG['upload_dir']
+    upload_dir = APP_CONFIG['upload_dir']
     processed_dir = APP_CONFIG['processed_dir']
     inbound_dir = APP_CONFIG['inbound_dir']
-      if !Dir.exists? processed_dir then
-        FileUtils.makedirs processed_dir
-      end
+    
+    if !Dir.exists? processed_dir then
+      FileUtils.makedirs processed_dir
+    end
+  
     if !Dir.exists? inbound_dir then
         FileUtils.makedirs inbound_dir
-      end
+    end
 
     @raw_files = RawFile.where("status = 'SENT'")
+    
     @raw_files.each do |raw_file|
-    begin
       filename = raw_file.path
       file = File.join(upload_dir, filename)
       processed_file = File.join(processed_dir, filename)
       filename_yml = filename + '.yml'
       inbound_file = File.join(inbound_dir, filename_yml)
 
-      case File.extname(file)
-        when '.xls'
-          inbound = Excel.new(file)
-        when '.csv'
-          inbound = CSV.read(file).to_yaml
-        else
+      begin
+        inbound = Excel.new(file) 
+      rescue 
+        logger.info("#{raw_file.id} - #{file} is not a Excel file.")
+        
+        begin
+          inbound = CSV.read(file)  
+        rescue 
+          logger.info("#{raw_file.id} - #{file} is not a CSV file.")
+        end
+        
       end
-
 
       content = inbound.to_yaml
 
       if inbound.is_a? Excel then
-      inbound.get_workbook.get_io.close
+        inbound.get_workbook.get_io.close
       end
 
       File.open(inbound_file, 'w') {|f| f.write(content) }
@@ -56,12 +61,8 @@ class RawFileTransformer < ActiveRecord::Base
       FileUtils.mv file, processed_file
       ActiveRecord::Base.connection.close
       true
-    rescue Exception => e
-      logger.fatal e
     end
-    end
-    rescue Exception => e
+  rescue Exception => e
     logger.fatal e
-    end
   end
 end
