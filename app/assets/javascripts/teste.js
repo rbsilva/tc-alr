51e77830c42247e01709116f77f4f1978ad688f2
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-10-10 20:15:57 -0300

    Testes com dashboard.

--- app/assets/javascripts/teste.js
+++ app/assets/javascripts/teste.js
@@ -1,20 +1,42 @@
 $(function() {
         var $dashboard_facts = $( "#dashboard_facts" ),
-            $dashboard_report = $( "#dashboard_report" );
- 
+            $dashboard_report = $( "#dashboard_report" ),
+            $dashboard_inbounds = $("#dashboard_inbounds");
+
         $( "th", $dashboard_facts ).draggable({
             helper: "clone",
             cursor: "move"
         });
- 
+
+        $( "td", $dashboard_inbounds ).draggable({
+          helper: "clone",
+          cursor: "move"
+        });
+
         $dashboard_report.droppable({
             accept: "#dashboard_facts table th",
             drop: function( event, ui ) {
                 loadReport( ui.draggable );
             }
         });
- 
+
+        $("th", $dashboard_facts).droppable({
+            accept: "#dashboard_inbounds table td",
+            drop: function( event, ui ) {
+                loadFact( ui.draggable, $(this) );
+            }
+        });
+
         function loadReport( $item ) {
-			$("td", $dashboard_report).text($item.clone().html());
+          $("td", $dashboard_report).text($item.clone().html());
+        }
+
+        function loadFact( $item, $column ) {
+          var $index = $item.index() + 1;
+          var $table = $item.parent().parent();
+          $table.find('td:nth-child('+$index+')').each( function(){
+            $column.parent().parent().append('<tr><td>'+$(this).html()+'</td></tr>');
+            console.log($index + ' ' + $table.find('td:nth-child('+$index+')').html());
+          });
         }
     });
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,20 @@
 class Admin::DashboardController < BaseController
   def index
+    require 'xmlsimple'
+
     @facts = DataWarehouse.facts
+
+    @inbounds = []
+
+    @raw_files = RawFile.where("status = 'PROCESSED'")
+
+    @raw_files.each do |raw_file|
+      begin
+        file = Inbound.where("raw_file_id = #{raw_file.id}").first.file
+        @inbounds << [XmlSimple.xml_in(ActiveRecord::Base.connection.unescape_bytea(file)), raw_file]
+      rescue SyntaxError, StandardError
+        @inbounds << [file, raw_file, $!] rescue @inbounds << ['',raw_file, $!]
+      end
+    end
   end
 end
