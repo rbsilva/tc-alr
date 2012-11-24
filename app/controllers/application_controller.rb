f3a52fc2d1bf79274989fd65855645f08dfce296
Rodrigo Batista da Silva <rbsilva.ti@gmail.com>
2012-11-23 22:44:18 -0200

    Correção para pegar erros e renderizar uma mensagem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # Code used by CanCan to manage exception
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = exception.message
-    redirect_to root_url
-  end
-end
+  rescue_from Exception, :with => :handle_exceptions
 
+  private
+    def handle_exceptions(exception)
+      if exception.class == CanCan::AccessDenied then
+        flash[:error] = exception.message
+        logger.fatal exception.message
+        redirect_to root_url
+      else
+        logger.fatal "AQUI MANO, PEGA! --> #{exception} #{exception.backtrace}"
+        #ErrorMailer.notify_error(exception, params).deliver
+        #render 'error_mailer/500', :status => 500, :locals => {:error => exception}
+        flash[:error] = exception.message
+      end
+    end
+end
